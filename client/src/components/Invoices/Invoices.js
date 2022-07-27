bab7a73a95a1843c28574e228db5543cefce6075
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-07-27 16:54:26 +0000

    filter svg fix 2

--- client/src/components/Invoices/Invoices.js
+++ client/src/components/Invoices/Invoices.js
@@ -15,7 +15,7 @@ const Invoices = (props) => {
             </div>
             <div className='right-section'>
                 <div className='dropdown'>
-                <label className='filter-text'>Filter <span>by status</span></label>
+                <span className='filter-text'>Filter <span>by status</span></span>
                 <svg width="11" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l4.228 4.228L9.456 1" stroke="#7C5DFA" strokeWidth="2" fill="none" fillRule="evenodd"/></svg>
                 </div>
                 <div className='btn'>
