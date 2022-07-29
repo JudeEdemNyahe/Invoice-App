fa53bee03caa44f1d6f3d2e2b1d5a08b2e0b43ec
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-07-29 11:40:06 +0000

    Added 404 not found errors

--- server/app.js
+++ server/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
-const invoiceRouter = require('./routes/invoiceRouter');
-const cors = require('cors');
-const AppError = require('./utils/apiError');
-const globalErrorhandler = require('./controllers/errorController');
+const cors = require("cors");
+const invoiceRouter = require("./routes/invoiceRouter");
+const AppError = require("./utils/apiError");
+const globalErrorhandler = require("./controllers/errorController");
 
 //Development logging
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
 }
 
 //Body Parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 app.use(cors());
-
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -22,22 +22,18 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/v1/invoices', invoiceRouter);
-
+app.use("/api/v1/invoices", invoiceRouter);
 
 // if an endpoint hits this middle ware an error is thrown with the message
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
     // res.status(404).json({
     //     status: 'fail',
     //     message: `Can't find ${req.originalUrl} on this server`
     // })
 
-
     next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-
 });
 
-app.use(globalErrorhandler)
+app.use(globalErrorhandler);
 
-
-module.exports = app
+module.exports = app;
