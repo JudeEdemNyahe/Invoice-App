5da89a24f666448a70e3e36065ce94fd5db4f006
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-08-02 10:22:05 +0000

    useEffect import removed

--- client/src/components/Invoices/Invoices.js
+++ client/src/components/Invoices/Invoices.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState} from 'react';
 
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
--- client/src/components/ViewInvoice/ViewInvoice.js
+++ client/src/components/ViewInvoice/ViewInvoice.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import EditInvoice from '../Edit Invoice/EditInvoice';
 
