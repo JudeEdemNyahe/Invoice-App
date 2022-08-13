b2c9cc0f8e70e59356bb75098cd3bfd371a37786
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-08-13 00:05:30 +0000

    new invoice form close on click outside

--- client/src/components/Invoices/Invoices.js
+++ client/src/components/Invoices/Invoices.js
@@ -160,20 +160,25 @@ const Invoices = () => {
                 </div>
               </div>
             </OutsideClickHandler>
-            <div className="btn">
-              <button onClick={() => setShowNewInvoice((prev) => !prev)}>
-                <span className="plus-icon">
-                  <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z"
-                      fill="#7C5DFA"
-                      fillRule="nonzero"
-                    />
-                  </svg>
-                </span>
-                New<span className="remove1">Invoice</span>
-              </button>
-            </div>
+            <OutsideClickHandler
+              onOutsideClick={() => {
+                setShowNewInvoice(false);
+              }}>
+              <div className="btn">
+                <button onClick={() => setShowNewInvoice((prev) => !prev)}>
+                  <span className="plus-icon">
+                    <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z"
+                        fill="#7C5DFA"
+                        fillRule="nonzero"
+                      />
+                    </svg>
+                  </span>
+                  New<span className="remove1">Invoice</span>
+                </button>
+              </div>
+            </OutsideClickHandler>
           </div>
         </div>
         {invoices?.length > 0 ? ( //if there invoices in our database
