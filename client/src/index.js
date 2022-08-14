b085d110db433a53c2876c3c895c4666015a806c
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-08-14 19:09:46 +0000

    library removed

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,10 @@ import App from './App'
 import './index.css'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import reduxReset from 'redux-reset'
 import thunk from 'redux-thunk';
 
 import reducers from './reducers'
-const store = createStore(reducers, compose(applyMiddleware(thunk), reduxReset()))
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 const root = createRoot(document.getElementById('root'));
 root.render(
